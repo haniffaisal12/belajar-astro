5fdddd6ea4c083425b056c2c5b2dd3c10e4f7a2a
haniffaisal12 <hanifarifinfaisal@gmail.com>
2024-01-22 16:43:01 +0700

    added an RSS feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://belajar-astro-haniffaisal12.netlify.app"
+});
